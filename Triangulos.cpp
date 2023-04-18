a748e44b260f0d900f24b637d9770c684dc714b4
RafaelSFiamengue <127798981+RafaelSFiamengue@users.noreply.github.com>
2023-04-17 22:03:59 -0300

    Update Triangulos.cpp

--- Triangulos.cpp
+++ Triangulos.cpp
@@ -1,42 +1,63 @@
+//Bibliotecas usadas
 #include <iostream>
 #include <math.h>
 using namespace std;
 
-//Função criada para calcular a area do triangulo
-double calcularAreaTriangulo (double a, double b, double c) {
-    double p = (a + b + c) / 2;
-    return sqrt(p * (p - a) * (p - b) * (p-c));
-    
-}
-int main() {
-    double xa, xb, xc, ya, yb, yc;
-    
-    //Pede os valores dos lados e lê os valores para as variaveis
-    cout << "Digite as medidas do triângulo X: ";
-    cin >> xa >> xb >> xc;
-    
-    //Pede os valores dos lados e lê os valores para as variaveis
-    cout << "Digite as medidas do triângulo Y: ";
-    cin >> ya >> yb >> yc;
-    
-    //Chama a função da area para calcular a area dos triangulos
-    double areaX = calcularAreaTriangulo(xa, xb, xc);
-    double areaY = calcularAreaTriangulo(ya, yb, yc);
-    
-    //Imprime os valores das areas para o usuario
-    cout << "A área do triangulo X é: " << areaX << endl;
-    cout << "A área do triangulo Y é: " << areaY <<endl;
-    
-    //Condicional para ver qual tem a maior area, se forem diferentes.
-    if (areaX > areaY) {
-        cout << "O triangulo X tem a maior area. " << endl;
-    }
-    else if (areaY > areaX) {
-        cout << "O triangulo Y tem a maior area. " << endl;
+//Classe do Triangulo
+class Triangulo {
+private:
+    double a, b, c;
+public:
+    //Variaves dos lados adquirindo o valor de a, b, c
+    Triangulo(double lado1, double lado2, double lado3) {
+        a = lado1;
+        b = lado2;
+        c = lado3;
     } 
-    else {
-        cout << "Os triangulos possuem a mesma area. " << endl;
+    //Calculo da area do triangulo
+    double calculoArea () {
+        double p = (a + b + c) / 2.0;
+        double area = sqrt (p * (p - a) * (p - b) * (p - c));
+        return area;
     }
-
+    //Torna os valores de lado para os valores de letra
+    double getlado1() { return a; }
+    double getlado2() { return b; }
+    double getlado3() { return c; }
+};
+//Programa principal
+int main() {
+    //Variaveis dos lados
+    double a, b, c;
+    
+    //Pergunta para o usuario os valor dos lados e faz a leitura dos valores
+    cout << "Digite os lados do triangulo X: ";
+    cin >> a >> b >> c;
+    Triangulo X(a, b, c);
+    
+    //Pergunta para o usuario os valor dos lados e faz a leitura dos valores
+    cout << "Digite os lados do triangulo Y: ";
+    cin >> a >> b >> c;
+    Triangulo Y(a, b, c);
+    
+    //Calculo das areas de cada triangulo
+    double areaX = X.calculoArea();
+    double areaY = Y.calculoArea();
+    
+    //Imprime o valor da area de cada triangulo
+    cout << "Area do triangulo X é: " << areaX << endl;
+    cout << "Area do triangulo Y é: " << areaY << endl;
+    
+    //Condicional para ver qual triangulo tem a maior area
+    if (areaX > areaY) {
+        cout << "A area do X é a maior area" << endl;
+    }
+    else if (areaX < areaY) {
+        cout << "A area do Y é a maior area" << endl;
+    }
+    else {
+        cout << "As areas são iguais" << endl;
+    }
+    
     return 0;
 }
